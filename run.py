f6477a75abad48446d9d523376bcc6c4205ca3fd
Rick Anderson <rick@byobwebsite.com>
2018-04-22 09:51:10 -0700

    google oauth creds added

--- run.py
+++ run.py
@@ -1,15 +1,7 @@
 from winejournal.app import create_app
 
-import os
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
-
-
 app = create_app()
-
-
 
 if __name__ == '__main__':
     app.debug = app.config['DEBUG']
     app.run(host='0.0.0.0')
-
